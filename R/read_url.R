0b10cb26914c65ef51af106d1e1bbc9852e559cb
rqthomas <rqthomas@vt.edu>
2024-08-21 16:20:00 -0400

    longer backoff

--- R/read_url.R
+++ R/read_url.R
@@ -1,6 +1,6 @@
 read_url <- function(url_base, url_path){
 
-  out <- httr2::request(url_base) |> httr2::req_url_path(url_path) |> httr2::req_retry(max_tries = 5, backoff = ~ 5) |> httr2::req_perform() |> httr2::resp_body_json(simplifyVector = TRUE)
+  out <- httr2::request(url_base) |> httr2::req_url_path(url_path) |> httr2::req_retry(max_tries = 5, backoff = ~ 20) |> httr2::req_perform() |> httr2::resp_body_json(simplifyVector = TRUE)
 
   return(out)
 }
