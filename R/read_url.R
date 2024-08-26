aca9f05ae0b33375768461dafae17c1f476145c0
rqthomas <rqthomas@vt.edu>
2024-08-26 19:08:55 -0400

    adding in jsonlite read because it seems to work on mac consistently

--- R/read_url.R
+++ R/read_url.R
@@ -1,10 +1,20 @@
 read_url <- function(url_base, url_path){
 
+
+  os <- Sys.info()[['sysname']]
+
+  if(os != "Darwin"){
+    #for some reason jsonlite fails on windows
+
   out <- httr2::request(url_base) |>
     httr2::req_url_path_append(url_path) |>
     httr2::req_throttle(10 / 60, realm = url_base) |>
     httr2::req_perform() |>
     httr2::resp_body_json(simplifyVector = TRUE)
+  }else{
+    # and httr2 fails on mac
+    out <- jsonlite::read_json(file.path(url_base,url_path), simplifyVector = TRUE)
+  }
 
   #out <- httr2::request(url_base) |>
   #  httr2::req_url_path_append(url_path) |>
